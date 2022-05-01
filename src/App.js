8fd698fb2939c13bb8a19cc3b98b7ae61f373601
hussin soli <hisokasama68@gmail.com>
2022-05-01 14:08:01 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import CountryCardsGrid from "./components/CountryCardsGrid.js";
+import Filter from "./components/Filter";
+import SearchBar from "./components/SearchBar.js";
+import Spinner from "./components/shared/spinner.js";
+import { useCountries } from "./data/useCountries.js";
+import Header from "./components/Header";
+import Country from "./components/CountryDetail";
 function App() {
+  const { loading, dark, countryDetail, countries } = useCountries();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={dark ? "dark" : ""}>
+      <div className="w-100% h-screen dark:bg-darkBg bg-lightBg overflow-x-hidden">
+        <Header />
+        {countryDetail.isClicked ? (
+          <Country data={countries[countryDetail.index]} />
+        ) : (
+          <main className="w-screen h-screen  space-y-12 > *+* px-10 ">
+            <div className="flex justify-between items-center flex-col sm:flex-row ">
+              <SearchBar />
+              <Filter />
+            </div>
+            {loading ? (
+              <CountryCardsGrid />
+            ) : (
+              <div className="grid place-items-center w-full h-full">
+                <Spinner />
+              </div>
+            )}
+          </main>
+        )}
+      </div>
     </div>
   );
 }
